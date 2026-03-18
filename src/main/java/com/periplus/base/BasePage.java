cc8ac944f92477e38628c6c719eb73029d847f8f
Muhammad Raihaan Perdana <perdanaraihan96@gmail.com>
2026-03-19 01:43:14 +0700

    feat: add functionalities in base page and methods in base tests

--- src/main/java/com/periplus/base/BasePage.java
+++ src/main/java/com/periplus/base/BasePage.java
@@ -1,35 +1,106 @@
 package com.periplus.base;
 
+import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
+import java.util.logging.Logger;
+import utilities.WaitUtility;
 
-public class BasePage {
+public abstract class BasePage {
     protected WebDriver driver;
+    protected WebDriverWait wait;
+    protected final Logger logger = Logger.getLogger(this.getClass().getName());
     private static final int DEFAULT_TIMEOUT = 10;
 
     public BasePage(WebDriver driver) {
         this.driver = driver;
+        this.wait = new WebDriverWait(driver, Duration.ofSeconds(DEFAULT_TIMEOUT));
+        PageFactory.initElements(driver, this);
+        waitForPageReady();
+    }
+
+    private void waitForPageReady() {
+        WaitUtility.waitForPreloaderInvisible(driver);
     }
 
     protected WebElement find(By locator) {
+        waitForPageReady();
         return new WebDriverWait(driver, Duration.ofSeconds(DEFAULT_TIMEOUT))
                 .until(ExpectedConditions.visibilityOfElementLocated(locator));
     }
 
     protected void set(By locator, String text) {
+        waitForPageReady();
         WebElement element = find(locator);
         element.clear();
         element.sendKeys(text);
     }
 
     protected void click(By locator) {
+        waitForPageReady();
         new WebDriverWait(driver, Duration.ofSeconds(DEFAULT_TIMEOUT))
                 .until(ExpectedConditions.elementToBeClickable(locator))
                 .click();
+    }
+
+    protected void hover(By locator) {
+        waitForPageReady();
+        WebElement element = find(locator);
+        Actions actions = new Actions(driver);
+        actions.moveToElement(element).perform();
+    }
+
+    protected void waitForElementVisible(By locator) {
+        wait.until(ExpectedConditions.visibilityOfElementLocated(locator));
+    }
+
+    protected boolean isElementPresent(By locator) {
+        return !driver.findElements(locator).isEmpty();
+    }
+
+    protected WebElement findWithin(WebElement parent, By childLocator) {
+        waitForPageReady();
+        WebElement child = parent.findElement(childLocator);
+        return new WebDriverWait(driver, Duration.ofSeconds(DEFAULT_TIMEOUT))
+                .until(ExpectedConditions.visibilityOf(child));
+    }
+
+    protected void clickWithin(WebElement parent, By childLocator) {
+        waitForPageReady();
+        WebElement child = parent.findElement(childLocator);
+        new WebDriverWait(driver, Duration.ofSeconds(DEFAULT_TIMEOUT))
+                .until(ExpectedConditions.elementToBeClickable(child))
+                .click();
+    }
+
+    protected void setWithin(WebElement parent, By childLocator, String text) {
+        WebElement element = findWithin(parent, childLocator);
+        element.clear();
+        element.sendKeys(text);
+    }
+
+    protected boolean isAlertPresent() {
+        try {
+            driver.switchTo().alert();
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    protected void acceptAlert() {
+        try {
+            Alert alert = driver.switchTo().alert();
+            logger.info("Alert detected: " + alert.getText());
+            alert.accept();
+        } catch (Exception e) {
+        }
     }
 }
--- src/test/java/com/periplus/base/BaseTest.java
+++ src/test/java/com/periplus/base/BaseTest.java
@@ -1,34 +1,101 @@
 package com.periplus.base;
 
+import com.periplus.pages.CartPage;
+import com.periplus.pages.HomePage;
+import com.periplus.pages.LoginPage;
+
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.BeforeClass;
-import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.*;
+import utilities.ConfigManager;
 import utilities.DriverUtility;
 
 import java.time.Duration;
+import java.util.logging.Logger;
 
 public class BaseTest {
-    private WebDriver driver;
-    protected BasePage basePage;
-    protected String url = "https://www.periplus.com/";
+    protected WebDriver driver;
+    protected HomePage homePage;
+    protected Logger logger = Logger.getLogger(this.getClass().getName());
+
+    protected String baseUrl = ConfigManager.getProperty("app.url", "https://www.periplus.com/");
+    protected String testEmail = ConfigManager.getProperty("test.email");
+    protected String testPassword = ConfigManager.getProperty("test.password");
+
+    private By closeIcon = By.xpath("//i[@class='ti-close']");
 
     @BeforeClass
     public void setUp() {
         driver = DriverUtility.createDriver();
         driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(30));
+        driver.manage().window().maximize();
+        logger.info("WebDriver initialized successfully");
     }
 
     @BeforeMethod
     public void loadApplication() {
-        driver.get(url);
-        basePage = new BasePage(driver);
+        driver.get(baseUrl);
+        homePage = new HomePage(driver);
+        logger.info("Navigated to: " + baseUrl);
+    }
+
+    @BeforeMethod(dependsOnMethods = "loadApplication")
+    public void ensureLoggedIn() {
+        closeModalIfPresent();
+
+        if (homePage.isLoggedIn()) {
+            logger.info("User already logged in, skipping login");
+            return;
+        }
+
+        LoginPage loginPage = homePage.goToLoginPage();
+        loginPage.login(testEmail, testPassword);
+        logger.info("Login completed");
+    }
+
+    private void closeModalIfPresent() {
+        try {
+            driver.findElement(closeIcon).click();
+        } catch (Exception e) {
+        }
+    }
+
+    @AfterMethod
+    public void cleanupAfterTest() {
+        try {
+            driver.get(baseUrl + "checkout/cart");
+            CartPage cartPage = new CartPage(driver);
+
+            if (!cartPage.hasItems()) {
+                logger.info("Cart already empty, no cleanup needed");
+                return;
+            }
+
+            int maxAttempts = 3;
+            int attempts = 0;
+
+            while (cartPage.hasItems() && attempts < maxAttempts) {
+                cartPage.clearCart();
+                driver.navigate().refresh();
+                cartPage = new CartPage(driver);
+                attempts++;
+            }
+
+            if (cartPage.hasItems()) {
+                logger.warning("Cart cleanup may not be complete after " + maxAttempts + " attempts");
+            } else {
+                logger.info("Cart cleared successfully after test");
+            }
+        } catch (Exception e) {
+            logger.warning("Cleanup issue: " + e.getMessage());
+        }
     }
 
     @AfterClass
     public void tearDown() {
         if (driver != null) {
             driver.quit();
+            logger.info("WebDriver closed successfully");
         }
     }
 }
